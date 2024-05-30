2e91e9fd8a3b0d29264c133931969ebbb3e445d5
kariukijoni <kariukijoni.com>
2024-05-30 22:37:05 +0300

    code refractor

--- customers/views.py
+++ customers/views.py
@@ -12,8 +12,8 @@ def upload_file(request):
             file = request.FILES['file']
             df = pd.read_excel(file)
             for index, row in df.iterrows():
-                your_model_instance = Customers(code=row['Code'], name=row['Name'], tel=row['Tel'], route=row['Route'])
-                your_model_instance.save()
+                customers = Customers(code=row['Code'], name=row['Name'], tel=row['Tel'], route=row['Route'])
+                customers.save()
             return HttpResponse('File uploaded successfully!')
     else:
         form = UploadFileForm()
