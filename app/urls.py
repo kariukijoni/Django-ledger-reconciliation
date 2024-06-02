07d24ee96cabeece123c77c3aa2eed030511bce8
kariukijoni <kariukijoni.com>
2024-06-02 15:03:30 +0300

    refractor

--- app/urls.py
+++ app/urls.py
@@ -7,7 +7,7 @@ from .views import separate_narrative, upload_file,transaction_list_view,transac
 urlpatterns = [
 
     path('transaction_upload/', upload_file, name='transaction_upload_file'),
-    path('separate/', separate_narrative, name='transactions'),
+    path('separate/', separate_narrative, name='separate_narrative'),
     path('transactions/', transaction_list_view, name='transaction_list'),
     path('api/transactions/', transaction_list_data, name='transaction_list_data'),
 
